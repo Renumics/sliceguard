e3f19d6479f3b0c44d38ab6a6be45b301bb254fc
Daniel Klitzke <daniel.klitzke@renumics.com>
2023-06-20 16:26:00 +0200

    begin setting up structure

---
+++ segment_guard/segment_guard.py
@@ -0,0 +1,6 @@
+class SegmentGuard:
+
+
+    def find_segments(data, features=[], metadata=[], predictions=[], labels=[], metric=[]):
+        pass
+    
---
+++ tests/test_segment_guard.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from segment_guard import SegmentGuard
+
+
+def test_segment_guard():
+    df = pd.read_json("tests/predictions_embs.json")
+    print(df.columns)
+
+    sg = SegmentGuard()
+    sg.find_segments(df)
+
+
+
+if __name__ == "__main__":
+    test_segment_guard()

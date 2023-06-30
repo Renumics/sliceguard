7f336f8d0b53499695cbf5b6af3510885ac2695d
Daniel Klitzke <daniel.klitzke@renumics.com>
2023-06-30 11:42:16 +0200

    implement audio embedding generation

--- segment_guard/embedding_utils.py
+++ segment_guard/embedding_utils.py
@@ -1,10 +1,11 @@
 # Embedding support for text, images, audio
 import pandas as pd
 from sentence_transformers import SentenceTransformer
-from transformers import AutoFeatureExtractor, ViTModel, ASTForAudioClassification
+from transformers import AutoFeatureExtractor, ViTModel, ASTModel
 from tqdm import tqdm
 from PIL import Image
 import numpy as np
+import torch
 import torchaudio
 
 def generate_text_embeddings(texts):
@@ -25,7 +26,7 @@ def generate_image_embeddings(image_paths, model_name="google/vit-base-patch16-2
             with open(image_path, "rb") as f:
                 image = Image.open(f)
                 inputs = feature_extractor(images=image.convert("RGB"), return_tensors="pt")
-                with torch.nograd():
+                with torch.no_grad():
                     outputs = model(**inputs)
                 emb = outputs.last_hidden_state[0,0].cpu().detach().numpy()
                 embeddings.append(emb)
@@ -40,20 +41,18 @@ def generate_image_embeddings(image_paths, model_name="google/vit-base-patch16-2
 
 def generate_audio_embeddings(audio_paths, model_name="MIT/ast-finetuned-audioset-10-10-0.4593"):
     feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
-    model = ViTModel.from_pretrained(
+    model = ASTModel.from_pretrained(
         model_name, output_hidden_states=True
     )
     embeddings = []
     for i, audio_path in enumerate(tqdm(audio_paths)):
         # try: 
         y, sr = torchaudio.load(audio_path)
-        y = y.mean(1)
-        print(y.shape)
-        print(sr)
+        y = y.mean(0) # convert to mono
+
         inputs = feature_extractor(y, sampling_rate=sr, return_tensors="pt")
-        with torch.nograd():
+        with torch.no_grad():
             outputs = model(**inputs)
-        print(outputs.size())
         emb = outputs.last_hidden_state[0,0].cpu().detach().numpy()
         
         embeddings.append(emb)
